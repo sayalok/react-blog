ea74e1e949938d9ae3b8007625b2ae60cda5d731
me <sayalok@yahoo.com>
2020-05-09 07:53:39 +0600

    restrcture the project

--- src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './BackDrop.css'
-
-const BackDrop = (props) => (
-    props.show ? <div className="BackDrop" onClick={props.clicked}></div> : null
-)
-
-export default BackDrop;
